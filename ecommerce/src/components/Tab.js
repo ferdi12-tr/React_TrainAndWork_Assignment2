9e68e428946094557cfb446276434e398f86a64c
fkoca <ferdikoca12@gmail.com>
2023-12-20 08:51:49 +0300

    filter with tab

--- ecommerce/src/components/Tab.js
+++ ecommerce/src/components/Tab.js
@@ -3,8 +3,18 @@ import ProductCard from './ProductCard'
 
 
 export default class Tab extends Component {
+
+    state = {
+        tag: 1,
+    }
+
+    filterWithTag = (products) => {
+        return products.filter((product) => product.tag === this.state.tag)
+    }
+
     render() {
         const { products, addToCartList } = this.props;
+        let filteredProducts = this.filterWithTag(products)
         return (
             <>
                 <div className="product-double-row-tab-area section-space">
@@ -15,13 +25,13 @@ export default class Tab extends Component {
                                     <div className="tab-product-navigation">
                                         <ul className="nav nav-tabs justify-content-center" role="tablist">
                                             <li className="nav-item">
-                                                <button onClick={() => console.log("tab1")} className="nav-link active" type="button" data-bs-toggle="tab" data-bs-target="#product-series-1" role="tab" aria-controls="product-series-1" aria-selected="true">New Arrivals</button>
+                                                <button onClick={() => this.setState({ tag: 1 })} className="nav-link active" type="button" data-bs-toggle="tab" data-bs-target="#product-series-1" role="tab" aria-controls="product-series-1" aria-selected="true">New Arrivals</button>
                                             </li>
                                             <li className="nav-item">
-                                                <button onClick={() => console.log("tab2")} className="nav-link" type="button" data-bs-toggle="tab" data-bs-target="#product-series-2" role="tab" aria-controls="product-series-2" aria-selected="false">Featured</button>
+                                                <button onClick={() => this.setState({ tag: 2 })} className="nav-link" type="button" data-bs-toggle="tab" data-bs-target="#product-series-2" role="tab" aria-controls="product-series-2" aria-selected="false">Featured</button>
                                             </li>
                                             <li className="nav-item">
-                                                <button onClick={() => console.log("tab3")} className="nav-link" type="button" data-bs-toggle="tab" data-bs-target="#product-series-3" role="tab" aria-controls="product-series-3" aria-selected="false">On Sale</button>
+                                                <button onClick={() => this.setState({ tag: 3 })} className="nav-link" type="button" data-bs-toggle="tab" data-bs-target="#product-series-3" role="tab" aria-controls="product-series-3" aria-selected="false">On Sale</button>
                                             </li>
                                         </ul>
                                     </div>
@@ -31,7 +41,7 @@ export default class Tab extends Component {
                                             aria-labelledby="product-tab-1">
                                             <div className="product-row-wrapper">
                                                 <div className="row">
-                                                    {products.map((product, index) => {
+                                                    {filteredProducts.map((product, index) => {
                                                         return <ProductCard addToCartList={addToCartList} key={index} product={product} />;
                                                     })}
                                                 </div>
